74a12c7426424669bf310b1f7845a91fdd037ee9
jzhuo <zhuojason1@gmail.com>
2020-05-05 03:32:26 -0400

    update normalization

--- normalize.py
+++ normalize.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 # Z-Score Normalization
-def normalize_data(dataList, mu, std):
+def normalize_data(dataList):
 
-	imageList = list(dataList)
+	imageList = np.array(dataList)
+	
+	mu = np.mean(imageList)
+	std = np.std(imageList)
+
+	imageList = list(imageList)
 
 	for index in range(len(imageList)):
 		imageList[index] = ( imageList[index] - mu ) / std
